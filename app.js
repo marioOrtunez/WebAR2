1d92e4bc090a1c602fd2819d7df019b5bdc06d8f
marioOrtunez <162109593+marioOrtunez@users.noreply.github.com>
2024-05-21 17:55:02 +0200

    pruebas

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var one =     document.querySelector('#one');
+var one = document.querySelector('#one');
 var two = document.querySelector('#two');
 var three = document.querySelector('#three');
+
 one.addEventListener('click', function () {
-          camera.emit('one');
-          });
+  // Aquí puedes agregar cualquier acción que desees cuando se haga clic en el elemento 'one'
+});
+
 two.addEventListener('click', function () {
-          camera.emit('two');
-                    });
+  // Aquí puedes agregar cualquier acción que desees cuando se haga clic en el elemento 'two'
+});
 
 three.addEventListener('click', function () {
-          camera.emit('three');
-            });
+  // Aquí puedes agregar cualquier acción que desees cuando se haga clic en el elemento 'three'
+});
